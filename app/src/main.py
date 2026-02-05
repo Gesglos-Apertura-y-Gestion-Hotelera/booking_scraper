78cb6e94ca3b18fd0568ed8d4cdd125d0d4fd00c
Luis Alberto Chavez Castro <lachavezca@unal.edu.co>
2026-02-05 12:21:46 -0500

    reubicando SCRAPER REGISTRY para tener una mejor visibilidad del main y haceerlo mas mantenible sosteniendo una uica responsabilidad

--- app/src/main.py
+++ app/src/main.py
@@ -7,64 +7,12 @@ from datetime import datetime
 from typing import Optional
 
 from core.chrome_driver import ChromeDriverFactory
+from core.scraper_registry import SCRAPER_REGISTRY
 from utils.enviar_sheets import enviar_sheets
 from utils.get_script_key import get_script_key
 from utils.get_dates import get_dates
 from utils.get_sheet_data import get_sheet_data
 from utils.logger import logger
-
-
-# Mapeo de script_key a clases de scraper
-SCRAPER_REGISTRY = {
-    'clientes_diario': {
-        'module': 'Web_Scraping_Clientes',
-        'class': 'ClientesDiarioScraper',
-        'requires_dates': False,
-        'data_param': 'hoteles',
-        'sheet_name': 'clientes',
-        'description': 'Scraping diario de clientes (hoy)'
-    },
-    'clientes_prevision': {
-        'module': 'Web_Scraping_Clientes_Adhoc',
-        'class': 'ClientesDiarioScraperAdHoc',
-        'requires_dates': True,
-        'data_param': 'hoteles',
-        'sheet_name': 'clientes',
-        'description': 'Scraping de clientes en rango de fechas'
-    },
-    'competencia_diario': {
-        'module': 'Web_Scraping_Competencia',
-        'class': 'CompetenciaDiarioScraper',
-        'requires_dates': False,
-        'data_param': 'competidores',
-        'sheet_name': 'competencia',
-        'description': 'Scraping diario de competencia (hoy)'
-    },
-    'competencia_prevision': {
-        'module': 'Web_Scraping_Competencia_Adhoc',
-        'class': 'CompetenciaDiarioScraperAdHoc',
-        'requires_dates': True,
-        'data_param': 'competidores',
-        'sheet_name': 'competencia',
-        'description': 'Scraping de competencia en rango de fechas'
-    },
-    'seguimiento_diario': {
-        'module': 'Web_Scraping_Daily_Tracking',
-        'class': 'DailyTrackingScraper',
-        'requires_dates': False,
-        'data_param': 'hoteles',
-        'sheet_name': 'ciudades',
-        'description': 'Scraping de seguimiento diario'
-    },
-    'personalizado': {
-        'module': 'Web_Scraping_Booking',
-        'class': 'BookingScraperPersonalizado',
-        'requires_dates': True,
-        'data_param': 'hoteles',
-        'sheet_name': 'lista_hoteles',
-        'description': 'Scraping personalizado de Booking'
-    }
-}
 
 
 def import_scraper_class(module_name: str, class_name: str):
