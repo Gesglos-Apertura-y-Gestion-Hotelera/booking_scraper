aba2d131f45f6d90f9061e08f4b8fa5e990ab3aa
Luis Alberto Chavez Castro <lachavezca@unal.edu.co>
2026-02-06 15:21:00 -0500

    ajuste a script_key como variable de entorno

--- app/src/utils/get_script_key.py
+++ app/src/utils/get_script_key.py
@@ -6,8 +6,14 @@ from .logger import  logger
 
 def get_script_key()->str:
     # Acepta 1 O MÁS parámetros
-    args = sys.argv[1:]  # Todos los parámetros
-    script_key = args[0]
+    script_key = os.getenv('SCRIPT_KEY')
     if not script_key:
-        script_key = os.getenv('SCRIPT_KEY')
+        try:
+            logger.error(f" ** Var ENV script_key : ->{script_key}<-")
+            args = sys.argv[1:]  # Todos los parámetros
+            script_key = args[0]
+
+        except Exception:
+            logger.error(f" ** script key error: ->{script_key}<-")
+            pass
     return script_key
